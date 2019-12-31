7f0ba088e5451ee0e944bf3eeccc1ef5159088dd
Andy Warzon <awarzon@trek10.com>
2019-12-31 13:20:57 -0500

    Improve script instructions

--- scripts/deployment-variables.sh
+++ scripts/deployment-variables.sh
@@ -4,18 +4,27 @@ set -euo pipefail
 
 # Variables:
 
-##### NOTE: THE BELOW VARIABLES MUST BE SET! ######
-# Name of an S3 bucket to be created -- MUST BE GLOBALLY UNIQUE!
-S3_BUCKET=MY-UNIQUE-BUCKETNAME
-# AWS Account IDs
-DEV_ACCOUNT_ID=123456789123
-PROD_ACCOUNT_ID=123456789123
-
-###### THESE VARIABLES CAN BE OPTIONALLY ADJUSTED ######
-# Prefix for files in S3 bucket. Default is fine for most scenarios.
-S3_PREFIX=AWS-CICD-Quickstart
-# A name for your CloudFormation stack 
+#### USED BY BOTH git-setup.sh and stack-deploy.sh
+#####################
+# A name for your CloudFormation stack
+# This can optionally be adjusted
 STACK_NAME=cicd-serverless
 
+##### ONLY USED BY git-setup.sh
+#####################
 # Relative path to local folder (that does not exist) to store git project 
+# This can optionally be adjusted
 LOCAL_REPO_FOLDER=../sample-project-codecommit
+
+##### ONLY USED BY stack-deploy.sh
+#####################
+# Name of an S3 bucket to be created -- MUST BE GLOBALLY UNIQUE!
+# This must be set to a new value
+S3_BUCKET=MY-UNIQUE-BUCKETNAME
+# AWS Account IDs
+# These must be set to the correct values
+DEV_ACCOUNT_ID=123456789123
+PROD_ACCOUNT_ID=123456789123
+# Prefix for files in S3 bucket. 
+# Default is fine for most scenarios, this can optionally be adjusted
+S3_PREFIX=AWS-CICD-Quickstart
